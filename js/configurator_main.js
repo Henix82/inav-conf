cbd3addd754b45518124b2f448080691d8c3c291
Henix <tolkomnesharil@mail.ru>
2026-05-24 18:31:13 +0700

    remove dev unlock all tabs

--- js/configurator_main.js
+++ js/configurator_main.js
@@ -122,21 +122,6 @@ $(function() {
 
         // Tabs
         var ui_tabs = $('#tabs > ul');
-
-        const DEV_SHOW_ALL_TABS = true;
-
-        if (DEV_SHOW_ALL_TABS) {
-            GUI.allowedTabs = [
-                ...new Set([
-                    ...GUI.defaultAllowedTabsWhenDisconnected,
-                    ...GUI.defaultAllowedTabsWhenConnected,
-                ]),
-            ];
-
-            $('#tabs .mode-connected').show().removeClass('is-hidden');
-            $('#tabs .mode-disconnected').show().removeClass('is-hidden');
-        }
-
         $('a', ui_tabs).on('click', function() {
 
             if ($(this).parent().hasClass("tab_help")) {
@@ -152,7 +137,7 @@ $(function() {
                 var tab = tabClass.substring(4);
                 var tabName = $(self).text();
 
-                if (!DEV_SHOW_ALL_TABS && tabRequiresConnection && !CONFIGURATOR.connectionValid) {
+                if (tabRequiresConnection && !CONFIGURATOR.connectionValid) {
                     GUI.log(i18n.getMessage('tabSwitchConnectionRequired'));
                     return;
                 }
@@ -162,7 +147,7 @@ $(function() {
                     return;
                 }
 
-                if (!DEV_SHOW_ALL_TABS && GUI.allowedTabs.indexOf(tab) < 0) {
+                if (GUI.allowedTabs.indexOf(tab) < 0) {
                     GUI.log(i18n.getMessage('tabSwitchUpgradeRequired', [tabName]));
                     return;
                 }
